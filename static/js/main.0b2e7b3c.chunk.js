c4712e32bd30d8aa5da77541eb393304e3eed384
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-07-17 11:53:25 +0300

    Updates

---
+++ static/js/main.0b2e7b3c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{4:function(t,o,e){"use strict";e.r(o);var n=e(0),c=e.n(n),r=e(2),i=e.n(r),s=(e(9),e(3));i.a.render(Object(s.jsx)(c.a.StrictMode,{}),document.getElementById("root"))},9:function(t,o){}},[[4,1,2]]]);
+//# sourceMappingURL=main.0b2e7b3c.chunk.js.map
